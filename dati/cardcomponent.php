9a6bf96f7a28c712fffd6108e4bf8e4b6baab17d
MATTEOTOSINI94 <matteotosini300@icloud.com>
2021-12-01 15:20:45 +0100

    Milstone uno

---
+++ dati/cardcomponent.php
@@ -0,0 +1,9 @@
+      <div class="col">
+                    <div id="card-style" class="card">
+                    <img src="<?php echo $img ?>" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $titolo ?></h5>
+                        <p class="card-text"> <?php echo $autore ?> <br><?php echo $genere ?></p>
+                    </div>
+                    </div>
+             </div>
